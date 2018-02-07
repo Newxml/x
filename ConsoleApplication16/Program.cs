a01eef688ed12960bf4c18a589ac05aaced289dc
xumenglin <xumenglin@LSDWZJDIT031>
2018-02-07 16:18:06 +0800

    委托

--- ConsoleApplication16/Program.cs
+++ ConsoleApplication16/Program.cs
@@ -6,18 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication16
 {
-    public delegate void GreetingDelegate(string name ,int aa);
+    public delegate void GreetingDelegate(string name, DateTime aa);
     public delegate void GreetingDelegate1(string name);
     class Program
     {
-
         private static void EnglishGreeting(string name)
         {
             Console.WriteLine("Morning, " + name);
         }
-        private static void EnglishGreeting(string name,int aa)
+        private static void EnglishGreeting(string name, DateTime aa)
         {
-            Console.WriteLine("Morning, " + name+"asd"+aa);
+            Console.WriteLine("Morning, " + name + " now" + aa);
         }
 
         private static void ChineseGreeting(string name)
@@ -26,19 +25,17 @@ namespace ConsoleApplication16
         }
 
         //注意此方法，它接受一个GreetingDelegate类型的方法作为参数
-        private static void GreetPeople(string name,int aa, GreetingDelegate MakeGreeting)
+        private static void GreetPeople(string name, DateTime aa, GreetingDelegate MakeGreeting)
         {
-            MakeGreeting(name,aa);
+            MakeGreeting(name, aa);
         }
-        private static void GreetPeople(string name,  GreetingDelegate1 MakeGreeting)
+        private static void GreetPeople(string name, GreetingDelegate1 MakeGreeting)
         {
             MakeGreeting(name);
         }
-
-
         static void Main(string[] args)
         {
-            GreetPeople("AA",1, EnglishGreeting);
+            GreetPeople("AA", DateTime.Now, EnglishGreeting);
             GreetPeople("阿斗", ChineseGreeting);
             Console.ReadKey();
         }
