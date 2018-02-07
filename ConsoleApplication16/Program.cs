4cedc1f53d7ebbd8380d000fd90352159aa33b1b
xumenglin <xumenglin@LSDWZJDIT031>
2018-02-07 16:12:50 +0800

    添加项目文件。

---
+++ ConsoleApplication16/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    public delegate void GreetingDelegate(string name ,int aa);
+    public delegate void GreetingDelegate1(string name);
+    class Program
+    {
+
+        private static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("Morning, " + name);
+        }
+        private static void EnglishGreeting(string name,int aa)
+        {
+            Console.WriteLine("Morning, " + name+"asd"+aa);
+        }
+
+        private static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("早上好, " + name);
+        }
+
+        //注意此方法，它接受一个GreetingDelegate类型的方法作为参数
+        private static void GreetPeople(string name,int aa, GreetingDelegate MakeGreeting)
+        {
+            MakeGreeting(name,aa);
+        }
+        private static void GreetPeople(string name,  GreetingDelegate1 MakeGreeting)
+        {
+            MakeGreeting(name);
+        }
+
+
+        static void Main(string[] args)
+        {
+            GreetPeople("AA",1, EnglishGreeting);
+            GreetPeople("阿斗", ChineseGreeting);
+            Console.ReadKey();
+        }
+    }
+}
